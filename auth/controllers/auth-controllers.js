b184dd2f072db031f511355217037aa3c465099b
ShadrackAdwera <adweshshaddie@gmail.com>
2021-11-18 07:07:07 +0300

    create login controller

--- auth/controllers/auth-controllers.js
+++ auth/controllers/auth-controllers.js
@@ -30,7 +30,11 @@ const signUp = async(req,res,next) => {
     }
     //create user
     const user = new User({
-        name, email, password: hashedPassword, role
+        name, 
+        email, 
+        password: hashedPassword, 
+        role, 
+        ticketsAssiged: [], resetToken: null, tokenExpiration: undefined
     })
     //save user
     try {
@@ -46,3 +50,37 @@ const signUp = async(req,res,next) => {
     }
     res.status(201).json({message: 'Sign up successful', user: { id: user._id.toString(), email, token }})
 }
+
+//TODO: Login controlller
+const login = async(req,res,next) => {
+    const { email, password } = req.body;
+
+    let foundUser
+    let isPassword
+    let token
+    try {
+        foundUser = await User.findOne({email},'-password').exec()
+    } catch (error) {
+        return next(new HttpError('An error occured, try again',500));
+    }
+    if(!foundUser) {
+        return next(new HttpError('This user does not exist, sign up', 404));
+    }
+    //check password
+    try {
+        isPassword = await bcrypt.compare(password, foundUser.password);
+    } catch (error) {
+        return next(new HttpError('Auth failed', 500))
+    }
+    //generate token
+    try {
+        token = jwt.sign({id: foundUser._id.toString(), email: foundUser.email}, 'supersecretkey', { expiresIn: '1h' });
+    } catch (error) {
+        return next(new HttpError('Auth failed', 500));
+    }
+    res.status(200).json({message: 'Sign Up Successful', user: { id: foundUser._id.toString(), email, token }})
+}
+
+//TODO: Generate password reset link controller
+
+//TODO: Reset password controller
